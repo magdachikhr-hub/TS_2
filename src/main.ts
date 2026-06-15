332a2e018183239697f0495541ef0bbb36b59331
magda chikhradz <magda292005@gmail.com>
2026-06-15 16:15:22 +0400

    getting input values

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 //  https://www.figma.com/design/2sEdZKMltXKCalHGx4AJwT/mortgage-repayment-calculator?node-id=7-2&p=f&t=SRl4UkkD6nXG8XL5-0
+
+const form = document.querySelector("form") as HTMLFormElement;
+const clearBtn = document.querySelector(".clear") as HTMLButtonElement;
+const calculateBtn = document.querySelector(".calc") as HTMLButtonElement;
+const termInput = document.getElementById("term") as HTMLInputElement;
+const rateInput = document.getElementById("rate") as HTMLInputElement;
+const mortgageInput = document.getElementById(
+  "mortgage_amount",
+) as HTMLInputElement;
+
+const monthlySum = document.getElementById("monthly_sum") as HTMLSpanElement;
+const totalSum = document.getElementById("total_sum") as HTMLSpanElement;
+const primaryChild = document.querySelector(".child") as HTMLDivElement;
+const resultHtml = document.querySelector(".result") as HTMLDivElement;
+
+interface mortgageInputs {
+  mortgageAmount: number;
+  mortgageTerm: number;
+  mortgageRate: number;
+  mortgageType: "repayment" | "interest";
+}
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  const mortgage: number = parseFloat(mortgageInput.value);
+  const term: number = parseInt(termInput.value);
+  const rate: number = parseFloat(rateInput.value);
+});
